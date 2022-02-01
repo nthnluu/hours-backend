df8b74e87be9e692cbeb6b0ecd581ebef04e8cb3
Nathan Benavides-Luu <nate1299@me.com>
2022-02-01 10:13:13 -0500

    Update client_init.ts

--- tests/src/client_init.ts
+++ tests/src/client_init.ts
@@ -3,12 +3,12 @@ import { initializeApp } from 'firebase/app'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: 'AIzaSyDMLmWBD_wdZG2VnmLGmZooLSVXM7t-vNo',
-    authDomain: 'signmeup-load-test.firebaseapp.com',
-    projectId: 'signmeup-load-test',
-    storageBucket: 'signmeup-load-test.appspot.com',
-    messagingSenderId: '292812123376',
-    appId: '1:292812123376:web:01b8ad93b4d74a47716635',
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
 }
 
 // Initialize Firebase
